8142d80788d0831830fd53033ed5db00748e6965
BenGauntRFI <ben.gaunt@rfi.ac.uk>
2021-12-02 10:26:29 +0000

    Added two functions to sonar.py, to correctly solve the day 1 puzzle.

---
+++ sonar.py
@@ -0,0 +1,36 @@
+import urllib.request
+
+input = 'inputs/day1_input.txt'
+
+
+def single_depths(input_file):
+   depths = []
+
+   with open(input_file) as file:
+      for line in file:
+         depths.append(int(line))
+
+   increases = 0
+   for i in range(len(depths) - 1):
+      if depths[i + 1] > depths[i]:
+         increases += 1
+
+   return increases
+
+def triple_depths(input_file):
+   depths = []
+
+   with open(input_file) as file:
+      for line in file:
+         depths.append(int(line))
+
+   increases = 0
+   for i in range(len(depths) - 3):
+      sum_i = depths[i] + depths[i + 1] + depths[i + 2]
+      sum_i_plus_one = depths[i + 1] + depths[i + 2] + depths[i + 3]
+      if sum_i_plus_one > sum_i:
+         increases += 1
+
+   return increases
+
+print(triple_depths(input))
