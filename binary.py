d4de13084be6c9c92b59435bbee0d752c77932d1
BenGauntRFI <ben.gaunt@rfi.ac.uk>
2021-12-03 11:35:31 +0000

    Added oxygen() and carbon() functions to binary.py, to try and solve part two of the challenge.
    Unfortunately it doesn't work yet.

--- binary.py
+++ binary.py
@@ -1,5 +1,6 @@
 import numpy as np
 input = 'inputs/day3_input.txt'
+test = 'inputs/day3_test.txt'
 
 
 def power(input_file):
@@ -40,12 +41,78 @@ def binary_flip(binary_array):
          flipped_array.append(1)
    return flipped_array
 
+def oxygen(input_file):
+   binaries = []
 
-gamma = power(input)
-decimal_gamma = binary_array_to_decimal(gamma)
+   with open(input_file) as file:
+      for line in file:
+         binary = []
+         for digit in line[:-1]:
+            binary.append(int(digit))
+         binaries.append(binary)
 
-epsilon = binary_flip(gamma)
-decimal_epsilon = binary_array_to_decimal(epsilon)
+   np_binaries = np.array(binaries)
+   binary_length = np_binaries.shape[1]
+   for i in range(binary_length):
+      dig_sum = np_binaries[:, i].sum()
+      remove = np.ones(np_binaries.shape[0], dtype=bool)
+      if dig_sum > np_binaries.shape[0] / 2:
+         for k, dig in enumerate(np_binaries[:, i]):
+            if not dig:
+               remove[k] = False
+      else:
+         for k, dig in enumerate(np_binaries[:, i]):
+            if dig:
+               remove[k] = False
 
-print("gamma = {}, epsilon = {}, power = {}".format(decimal_gamma, decimal_epsilon, decimal_gamma * decimal_epsilon))
+      np_binaries = np.delete(np_binaries, remove, axis=0)
 
+      if np_binaries.shape[0] == 1:
+         break
+
+   return np_binaries[0]
+
+def carbon(input_file):
+   binaries = []
+
+   with open(input_file) as file:
+      for line in file:
+         binary = []
+         for digit in line[:-1]:
+            binary.append(int(digit))
+         binaries.append(binary)
+
+   np_binaries = np.array(binaries)
+   binary_length = np_binaries.shape[1]
+   for i in range(binary_length):
+      dig_sum = np_binaries[:, i].sum()
+      remove = np.ones(np_binaries.shape[0], dtype=bool)
+      if dig_sum < np_binaries.shape[0] / 2:
+         for k, dig in enumerate(np_binaries[:, i]):
+            if not dig:
+               remove[k] = False
+      else:
+         for k, dig in enumerate(np_binaries[:, i]):
+            if dig:
+               remove[k] = False
+
+
+
+      np_binaries = np.delete(np_binaries, remove, axis=0)
+      if np_binaries.shape[0] == 1:
+         break
+
+   return np_binaries[0]
+
+
+ox = oxygen(test)
+c = carbon(test)
+print(ox)
+print(c)
+
+ox_decimal = binary_array_to_decimal(ox)
+c_decimal = binary_array_to_decimal(c)
+
+print(ox_decimal)
+print(c_decimal)
+print(ox_decimal * c_decimal)
